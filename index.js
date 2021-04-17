c2840cc095fe94529078b0ab9b8bbde43cd600a5
lara <quzabubaker@gmail.com>
2021-04-18 01:47:01 +0300

    sex command :flushed:

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function sexCommand(message, args) {
 
     const getsex = async () => {
         const data = fetch(`https://www.eporner.com/api/v2/video/search/?query=${search}&per_page=10&page=2&thumbsize=big&order=top-weekly&gay=1&lq=1&format=json`)
-        const res = res.json();
+        const res = data.json();
         message.channel.send({ files: [res.videos[Math.floor(Math.random() * res.videos.length)].default_thumb.src] });
     }
     getsex();
